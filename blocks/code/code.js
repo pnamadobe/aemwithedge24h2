33f4f43214d8304bcb68122f1b243f665dc8a45d
Om Gupta <om.gupta@teknopoint.in>
2024-07-08 17:50:16 +0530

    header dl added

--- blocks/code/code.js
+++ blocks/code/code.js
@@ -1,14 +1,17 @@
-export default async function decorate(block){
+import { mobileHeaderAnalytics } from "./code-analytics.js";
+
+export default async function decorate(block) {
     const titleData = block.children[0]?.querySelector('p')?.textContent.trim() || '';
     block.innerHTML = '';
-    if(titleData){
+    if (titleData) {
         const titleElement = document.createElement('div');
         titleElement.innerHTML = titleData;
         block.append(titleElement);
     }
 
-    const iframe =  block.querySelectorAll('iframe');
-    if(iframe.length != 0){
+    const iframe = block.querySelectorAll('iframe');
+    if (iframe.length != 0) {
         block.parentElement.classList.add('iframe-width');
     }
+    mobileHeaderAnalytics(block);
 }
