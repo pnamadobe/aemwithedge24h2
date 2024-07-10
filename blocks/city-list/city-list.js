a018993d89960890223a5075ecb3ef6a6cc9c307
Vaibhav sasulkar <vaibhav.sasulkar@teknopoint.local>
2024-07-10 20:51:41 +0530

    changes done

--- blocks/city-list/city-list.js
+++ blocks/city-list/city-list.js
@@ -5,7 +5,15 @@ export default async function decorate(block) {
     const resp = await fetchAPI("GET", url);
     const data = await resp.json();
 
-    let li = renderHelper(data.data, `<div class="forName">
+    let li = renderHelper(data.data.sort(function (a, b) {
+        if (a.text < b.text) {
+            return -1;
+        }
+        if (a.text > b.text) {
+            return 1;
+        }
+        return 0;
+    }), `<div class="forName">
         <li><a href="{url}" title="{text}">{text}</a></li>    
     </div>`)
     block.innerHTML = '<ul>' + li + '</ul>'
